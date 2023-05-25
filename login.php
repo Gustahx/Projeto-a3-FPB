1d6cde140bc279f68b5c34b8b28b047a4e722d14
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-05-25 11:17:14 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <link rel= "stylesheet" href= "estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto a3</title>
 </head>
