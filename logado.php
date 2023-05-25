623080e18fca44e21303b75242a70e34d3a85380
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-05-25 09:43:24 -0300

    Update logado.php

--- logado.php
+++ logado.php
@@ -1,3 +1,4 @@
+<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <?php
 include('conexao.php');
 $login= isset($_POST['login'])?$_POST['login']:'';
