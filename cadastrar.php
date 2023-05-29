89601b9b6cead314e992a97d9145528e55c27902
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-05-29 11:24:56 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,3 +1,17 @@
+<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
+<?php
+include('conexao.php');
+
+$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : '';
+
+$insert = "INSERT INTO login (nome, login, senha)
+            VALUES ('$nome', '$login', '$senha')";
+$query = mysqli_query($conexao, $insert);
+
+echo "Cadastro Realizado com sucesso!"
+?>
 <!DOCTYPE html>
 <html>
 <head>
