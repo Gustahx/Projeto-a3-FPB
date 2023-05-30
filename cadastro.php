30a38efad0f0a44da924b3ff9a6a8e65fd725016
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-05-30 11:21:54 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,4 @@
+<link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,17 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel= "stylesheet" href= "estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Projeto A3 MANHÃ</title>
 </head>
 <body>
     <center>
         <form id="cadastro" action="cadastrar.php" method="POST">
-            Nome Completo: <input type="text" name="nome" required><br>
-            Login: <input type="text" name="login" required><br>
-            Senha: <input type="password" name="senha" required><br><br>
+            Nome Completo : <input type="text" name="nome" required><br>
+            Data de Nascimento : <input type="date" name="data" required><br>
+            Login : <input type="text" name="login" required><br>
+            Senha : <input type="password" name="senha" required><br><br>
             <input type="submit" name="cadastrar" value="Cadastrar">
         </form>
-        <a href="index.php">Voltar</a>
+        <a href="index.php">Voltar</a><br><br>
+
+        <progress value="75" max="100"></progress>
     </center>
 </body>
 </html>
