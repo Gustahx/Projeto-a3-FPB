24e90e192c1f58f26e32fc296c569967c484aca6
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-05-30 11:20:25 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,20 @@
 <link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <link rel= "stylesheet" href= "estilo.css">
+    <link rel= "stylesheet" href= "estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto a3</title>
+    <title>Projeto A3 MANHÃ</title>
 </head>
 <body>
     <center>
          <form id="login" action="logado.php" method="POST">
-        Login: <input type="text" name="login"><br>
-        Senha: <input type="password" name="senha"><br>
-         <input type="submit" name="entrar" value="Enviar">
-
+        Login: <input type="text" name="login" required><br>
+        Senha: <input type="password" name="senha" required><br>
+         <input type="submit" name="entrar" value="Enviar"><br><br>
+        <progress value="25" max="100"></progress>
    
         </form>
     </center>
