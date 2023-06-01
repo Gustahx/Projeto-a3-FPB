068f11fa163bf84331c963e5350cdb7d0209f9ad
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-06-01 11:14:27 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,30 +1,32 @@
 <link rel="icon" type="image/png" href="https://www.shareicon.net/data/128x128/2015/10/03/111567_development_512x512.png"/>
 <?php
-include('conexao.php');
+include("conexao.php");
 
 $nome = isset($_POST['nome']) ? $_POST['nome'] : '';
 $login = isset($_POST['login']) ? $_POST['login'] : '';
 $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
 $insert = "INSERT INTO login (nome, login, senha)
-            VALUES ('$nome', '$login', '$senha')";
+            VALUES
+            ('$nome', '$login', '$senha')";
 $query = mysqli_query($conexao, $insert);
 
-echo "Cadastro Realizado com sucesso!"
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel= "stylesheet" href= "estilo.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" text="text/css" href="estilo.css">   
     <title></title>
 </head>
 <body>
     <center>
-        Cadastro Realizado com Sucesso <br><br>
-        <a href="index.php">Voltar</a>
+        <h2>Cadastro Realizado com Sucesso </h2><br><br>
+        <a href="index.php">Voltar</a><br>
+        <progress value="100" max="100"></progress>
     </center>
 </body>
 </html>
