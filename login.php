0477b96c4a26bd8538c6d7873dbd7159377629d4
Gustavo <127751205+Gustahx@users.noreply.github.com>
2023-06-01 11:13:04 -0300

    Update login.php

--- login.php
+++ login.php
@@ -3,20 +3,20 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel= "stylesheet" href= "estilo.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto A3 MANHÃ</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" text="text/css" href="estilo.css">   
+    <title>Projeto A3 - Programação e Modelagem Manhã</title>
 </head>
 <body>
     <center>
-         <form id="login" action="logado.php" method="POST">
-        Login: <input type="text" name="login" required><br>
-        Senha: <input type="password" name="senha" required><br>
-         <input type="submit" name="entrar" value="Enviar"><br><br>
-        <progress value="25" max="100"></progress>
-   
-        </form>
-    </center>
+		<h1>Login</h1>
+		<form id="form-login" action="logado.php" method="POST">
+			Login: <input type="text" name="login" required><br>
+			Senha: <input type="password" name="senha" required><br><br>
+			<input type="submit" name="entrar" value="Entrar"><br><br>
+			<progress value="25" max="100"></progress>
+		</form>
+	</center>
 </body>
 </html>
